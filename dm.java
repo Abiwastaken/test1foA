54c1562a3b59bba47a4c3b2b81d23a544d59fe5b
Abi-Aeera <mukhiyaa@uni.coventry.ac.uk>
2025-04-27 08:48:16 +0545

    this also done

--- dm.java
+++ dm.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 public class dm {
- public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int mul = 1;
-   do {
-    mul = (n % 10) * mul;
-    n = n / 10;
-   } while (n != 0);
-        if (mul == 0) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int mul = 1;
+        boolean containsZero = false;
+
+        do {
+            int digit = n % 10;
+            if (digit == 0) {
+                containsZero = true;
+                break;
+            }
+            mul *= digit;
+            n /= 10;
+        } while (n != 0);
+
+        if (containsZero) {
             System.out.println("0");
         } else {
             System.out.println(mul);
+        }
 
-
-   }
-    
-       
-    sc.close();
-    
-
- }   
+        sc.close();
+    }
 }
